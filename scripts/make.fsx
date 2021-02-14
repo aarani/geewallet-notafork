3a1083bdf07a0da9387b85980836fe16819e6eae
Afshin Arani <afshinarani79@gmail.com>
2021-02-14 15:39:12 +0330

    TE

--- scripts/make.fsx
+++ scripts/make.fsx
@@ -192,7 +192,7 @@ let GetTestAssembly suite =
 
     testAssembly
 
-let TwoProcessTestNames = ["GeewalletToGeewallet"; "Revocation"]
+let TwoProcessTestNames = ["GeewalletToGeewallet"; "Revocation"; "RevocationHack"]
 
 let RunTwoProcessTests() =
     let testAssembly = GetTestAssembly "EndToEnd"
