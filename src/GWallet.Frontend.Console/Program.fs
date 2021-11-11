nblockchain/geewallet

Backend(LN): implement revocation
Co-authored-by: Andrew Cann <shum@canndrew.org>

src/GWallet.Frontend.Console/Program.fs
@@ -382,12 +382,17 @@ let rec CheckArchivedAccountsAreEmpty(): bool =
 let rec ProgramMainLoop() =
     let accounts = Account.GetAllActiveAccounts()
     let channelStatusJobs: seq<Async<Async<seq<string>>>> = LayerTwo.GetChannelStatuses accounts
+    let revokedTxCheckJobs: seq<Async<Option<string>>> =
+        Lightning.ChainWatcher.CheckForChannelFraudsAndSendRevocationTx
+        <| accounts.OfType<UtxoCoin.NormalUtxoAccount>()
+    let revokedTxCheckJob: Async<array<Option<string>>> = Async.Parallel revokedTxCheckJobs
     let channelInfoInteractionsJob: Async<array<Async<seq<string>>>> = Async.Parallel channelStatusJobs
     let displayAccountStatusesJob =
         UserInteraction.DisplayAccountStatuses(WhichAccount.All accounts)
-    let channelInfoInteractions, accountStatusesLines =
-        AsyncExtensions.MixedParallel2 channelInfoInteractionsJob displayAccountStatusesJob
+    let channelInfoInteractions, accountStatusesLines, _ =
+        AsyncExtensions.MixedParallel3 channelInfoInteractionsJob displayAccountStatusesJob revokedTxCheckJob
         |> Async.RunSynchronously
+
 
     Console.WriteLine ()
     Console.WriteLine "*** STATUS ***"

aarani/geewallet-notafork

src/GWallet.Frontend.Console/Program.fs
@@ -449,11 +449,15 @@ let rec CheckArchivedAccountsAreEmpty(): bool =
 let rec ProgramMainLoop() =
     let activeAccounts = Account.GetAllActiveAccounts()
     let channelStatusJobs: seq<Async<Async<seq<string>>>> = LayerTwo.GetChannelStatuses activeAccounts
+    let revokedTxCheckJobs: seq<Async<Option<string>>> =
+        Lightning.ChainWatcher.CheckForChannelFraudsAndSendRevocationTx
+        <| activeAccounts.OfType<UtxoCoin.NormalUtxoAccount>()
+    let revokedTxCheckJob: Async<array<Option<string>>> = Async.Parallel revokedTxCheckJobs
     let channelInfoInteractionsJob: Async<array<Async<seq<string>>>> = Async.Parallel channelStatusJobs
     let displayAccountStatusesJob =
         UserInteraction.DisplayAccountStatuses(WhichAccount.All activeAccounts)
-    let channelInfoInteractions, accountStatusesLines =
-        AsyncExtensions.MixedParallel2 channelInfoInteractionsJob displayAccountStatusesJob
+    let channelInfoInteractions, accountStatusesLines, _ =
+        AsyncExtensions.MixedParallel3 channelInfoInteractionsJob displayAccountStatusesJob revokedTxCheckJob
         |> Async.RunSynchronously
 
     Console.WriteLine ()