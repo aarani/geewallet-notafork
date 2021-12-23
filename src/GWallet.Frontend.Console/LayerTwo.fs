su8898/geewallet

commit 1b5ac7ee6fc992e893a469640342d4780fcf548f

Frontend.Console(LayerTwo): slight readability improvement (status)

src/GWallet.Frontend.Console/LayerTwo.fs
@@ -706,7 +706,10 @@ module LayerTwo =
                 yield async {
                     return async {
                         return seq {
-                            yield sprintf "%A Lightning Status (%i active channels)" currency activeChannelCount
+                            if activeChannelCount > 0 then
+                                yield sprintf "%A Lightning Status (%i active channels):" currency activeChannelCount
+                            else
+                                yield sprintf "%A Lightning Status: 0 active channels" currency
                         }
                     }
                 }

aarani/geewallet-notafork

commit 3fe74c162a1a8777d649396a87c5de12c1871a3d

Frontend.Console: fix AskChannelId
Issue introduced in 1b5ac7ee6fc992e893a469640342d4780fcf548f

src/GWallet.Frontend.Console/LayerTwo.fs
@@ -136,7 +136,7 @@ module LayerTwo =
                 None
             else
                 match Int32.TryParse indexText with
-                | true, index when index < channelIds.Count() ->
+                | true, index when index > 0 && index <= channelIds.Count() ->
                     Some (channelIds.ElementAt (index - 1))
                 | _ ->
                     Console.WriteLine "Invalid option"