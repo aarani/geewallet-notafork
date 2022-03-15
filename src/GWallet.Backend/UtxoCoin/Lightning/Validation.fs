5dc59d0b8ed5b17155e88b77ef10caf80309481f
Afshin <afshin@arani.dev>
2022-03-15 15:50:58 +0330

    Backend, Tests, Frontend: support for anchor channels

---
+++ src/GWallet.Backend/UtxoCoin/Lightning/Validation.fs
@@ -0,0 +1,27 @@
+﻿namespace GWallet.Backend.UtxoCoin.Lightning
+
+open DotNetLightning.Serialization
+open DotNetLightning.Serialization.Msgs
+open ResultUtils.Portability
+
+open GWallet.Backend.FSharpUtil
+
+module Validation =
+    type internal InitMsgValidationError =
+        | NoAnchorSupport
+        interface IErrorMsg with
+            member self.Message =
+                match self with
+                | NoAnchorSupport ->
+                    "no anchor channel support found"
+            member __.ChannelBreakdown: bool =
+                false
+
+    let internal ValidateRemoteInitMsg (remoteInit: InitMsg): Result<unit, InitMsgValidationError> =
+        let hasAmchorSupport =
+            remoteInit.Features.HasFeature Feature.OptionAnchorZeroFeeHtlcTx
+
+        if not hasAmchorSupport then
+            Error InitMsgValidationError.NoAnchorSupport
+        else
+            Ok ()
